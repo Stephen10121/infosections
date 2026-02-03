cb9fe5a0d891dd2f64591333f44fd17fe08c246a
Stephen Gruzin <stephengrzn@gmail.com>
2026-02-02 22:05:32 -0800

    Removed the extra calendar padding in the image feeds

--- frontend/src/routes/ifeed/[slug]/+page.svelte
+++ frontend/src/routes/ifeed/[slug]/+page.svelte
@@ -109,7 +109,7 @@
                 {#each data.additionalCalendars as additionalCalendar (`anAdditionalCalendar${additionalCalendar.id}`)}
                     <Carousel.Item class="w-screen h-screen">
                         <AspectRatio ratio={16 / 9} class="relative max-w-screen max-h-screen aspect-video centered-div">
-                            <div id="cal-root" class="dark min-h-screen w-full p-6 bg-background relative">
+                            <div id="cal-root" class="dark min-h-screen w-full bg-background relative">
                                 <Calendar autoUpdate={false} events={data.events} displaySettings={additionalCalendar.displaySettings} timeZone={timeZone} filters={additionalCalendar.filters} />
                             </div>
                         </AspectRatio>
