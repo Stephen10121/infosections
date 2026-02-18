20d69d40c192e28b1bbb8b0df057f0049b796deb
Stephen Gruzin <stephengrzn@gmail.com>
2026-02-17 17:36:34 -0800

    Added a fake redirect to api endpoint for bots to get the redirect url without the cors issue

---
+++ frontend/src/routes/api/go/[slug]/+server.ts
@@ -0,0 +1,110 @@
+import type { DynamicURLModel, URLRefHits, WeekSheetTimeSlot } from "@/utils.js";
+import { error } from "@sveltejs/kit";
+import { config } from "dotenv";
+import { Temporal } from "temporal-polyfill";
+
+config();
+
+//This function records the ref data after we redirect the user as to not waste their time.
+async function recordRefHit(url: URL, record: DynamicURLModel, locals: App.Locals) {
+    setTimeout(async () => {
+        let ref = url.searchParams.get("ref");
+        if (!ref || ref.length === 0) return;
+
+        let newRef: URLRefHits[] = [];
+        let refFound = false;
+
+        for (let i=0;i<record.refs.length;i++) {
+            let currentRef = record.refs[i];
+            if (ref !== currentRef.name) {
+                newRef.push(currentRef);
+            } else {
+                newRef.push({
+                    name: ref,
+                    hits: currentRef.hits + 1
+                });
+                refFound = true;
+            }
+
+        }
+
+        if (!refFound) {
+            newRef.push({
+                name: ref,
+                hits: 1
+            });
+        }
+
+        try {
+            await locals.pb.collection('dynamic_url').update(record.id, {
+                refs: newRef,
+            }, {
+                headers: {
+                    "Authorization": "Bearer " + process.env.POCKETBASE_TOKEN!
+                }
+            });
+        } catch (err) {
+            console.log("Failed to update ref for dynamic url: ", err);
+        }
+    }, 1);
+}
+
+export async function GET({ params, locals, url }) {
+    const goID = params.slug;
+
+    let record: DynamicURLModel;
+    try {
+        record = await locals.pb.collection('dynamic_url').getOne(goID, {
+            headers: {
+                "Authorization": "Bearer " + process.env.POCKETBASE_TOKEN!
+            }
+        });
+    } catch (_err) {
+        return error(404, "Dynamic Link not found.");
+    }
+
+    // The url has been disabled by the owner.
+    if (record.disableURL) {
+        return error(404, "Dynamic Link not found.");
+    }
+
+    let linkToRedirect = record.defaultRedirectTo;
+
+    // If the dynamic_url owner enabled the override switch, we dont need to worry about anything else and just return the override URL
+    if (record.enableOverrideRedirect) {
+        linkToRedirect = record.overrideRedirectTo;
+    } else if (record.enableWeekSheet) {
+        let today = Temporal.Now.zonedDateTimeISO(record.timeZone);
+    
+        const todaysTimeSheet = record.weekSheet[today.dayOfWeek - 1];
+        const currentMinute = today.hour * 60 + today.minute;
+    
+        let currentTimeSheet: WeekSheetTimeSlot | null = null;
+    
+        for (let i=0;i<todaysTimeSheet.length;i++) {
+            let thisTimeSheet = todaysTimeSheet[i];
+            if (thisTimeSheet.startMinute <= currentMinute && currentMinute <= thisTimeSheet.endMinute) {
+                currentTimeSheet = thisTimeSheet;
+            }
+        }
+    
+        if (currentTimeSheet) {
+            linkToRedirect = currentTimeSheet.link;
+        }
+    }
+
+    recordRefHit(url, record, locals);
+    // return json({
+    //     linkToRedirect,
+    // });
+    
+    return new Response(undefined, {
+        status: 200,
+        
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Expose-Headers": "X-Redirect-To",
+            "X-Redirect-To": linkToRedirect
+        }
+    });
+}
