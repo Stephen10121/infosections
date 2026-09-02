01ee03c5dff7adeffb03c80ea1643dde193314f4
Stephen Gruzin <stephengrzn@gmail.com>
2026-09-02 15:07:29 -0700

    Made it possible to fetch the new plans and service types from the backend

---
+++ frontend/src/lib/service.util.ts
@@ -0,0 +1,46 @@
+import type { RecordModel } from "pocketbase";
+
+export interface ServiceTypeDBModel extends RecordModel {
+	owner: string;
+	frequency: string;
+	name: string;
+	sequence: number;
+	created: string;
+	updated: string;
+}
+
+export interface PlanItem {
+	description: string;
+	item_type: "song" | "header" | "media" | "item";
+	key_name: string;
+	length: number;
+	sequence: number;
+	service_position: "pre" | "post" | "during";
+	title: string;
+}
+
+export interface PlanDBModel extends RecordModel {
+	title: string;
+	owner: string;
+	service_type: string;
+	dates: string;
+	items_count: number;
+	multi_day: boolean;
+	needed_positions_count: number;
+	other_time_count: number;
+	plan_notes_count: number;
+	plan_people_count: number;
+	prefers_order_view: boolean;
+	public: boolean;
+	public_by_schedule: boolean;
+	rehearsable: boolean;
+	rehearsal_time_count: number;
+	reminders_disabled: boolean;
+	series_title: string;
+	service_time_count: number;
+	total_length: number;
+	items: PlanItem[];
+	sort_date: string;
+	created: string;
+	updated: string;
+}
---
+++ frontend/src/routes/(mainWebsite)/dashboard/services.remote.ts
@@ -0,0 +1,47 @@
+import type { PlanDBModel, ServiceTypeDBModel } from "@/service.util";
+import { getRequestEvent, query } from "$app/server";
+import { config } from "dotenv";
+import * as v from "valibot";
+
+config();
+
+export const getMyServiceTypes = query(async () => {
+	const { locals } = getRequestEvent();
+	let serviceTypes: ServiceTypeDBModel[] = [];
+
+	if (!locals.user) return serviceTypes;
+
+	try {
+		serviceTypes = await locals.pb.collection("service_type").getFullList({
+			filter: `owner="${locals.user.id}"`,
+			headers: {
+				Authorization: "Bearer " + process.env["POCKETBASE_TOKEN"]!
+			}
+		});
+	} catch (err) {
+		console.log("Failed to fetch service types.", err);
+	}
+
+	return serviceTypes;
+});
+
+export const getPlansByServiceType = query(v.string(), async (serviceID) => {
+	const { locals } = getRequestEvent();
+	let plans: PlanDBModel[] = [];
+
+	if (!locals.user) return plans;
+
+	try {
+		plans = await locals.pb.collection("calendars").getFullList({
+			filter: `owner="${locals.user.id}" && service_type="${serviceID}"`,
+			headers: {
+				Authorization: "Bearer " + process.env["POCKETBASE_TOKEN"]!
+			}
+		});
+	} catch (err) {
+		console.log("Failed to plans.", err);
+		return plans;
+	}
+
+	return plans;
+});
